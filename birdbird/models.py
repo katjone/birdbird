45a76c1857a38c4da7b20e1166df55d1f8ede47c
kkaatt <kjone203.e@gmail.com>
2019-06-07 15:34:18 -0700

    Increased charfield size

--- birdbird/models.py
+++ birdbird/models.py
@@ -7,18 +7,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Species(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    photo_url = models.CharField(max_length=250)
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=700)
+    photo_url = models.CharField(max_length=300)
 
     def __str__(self):
         return self.name
 
 class Sighting(models.Model):
     date = models.DateField(default=now)
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
     location = PlainLocationField(based_fields=['address'], zoom=15)
-    photo_url = models.CharField(max_length=250, blank=True, null=True)
+    photo_url = models.CharField(max_length=300, blank=True, null=True)
     bird = models.ForeignKey(Species, on_delete=False, related_name='sightings')
     observer = models.ForeignKey(User, on_delete=False, related_name='sightings', default=1 )
 
